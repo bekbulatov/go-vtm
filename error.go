3d5d72e092d69214283df38a3f2f88275d4a1b73
bekbulatov <alexander@bekbulatov.ru>
2017-04-02 11:35:07 +0100

    Show http code in errors

--- error.go
+++ error.go
@@ -24,41 +24,36 @@ const (
 
 // APIError represents a generic API error.
 type APIError struct {
-	// ErrCode specifies the nature of the error.
-	ErrCode int
-	message string
+	ErrCode  int // ErrCode specifies the nature of the error.
+	httpCode int
+	message  string
 }
 
 func (e *APIError) Error() string {
-	return fmt.Sprintf("VTM API error: %s", e.message)
+	return fmt.Sprintf("VTM API error %d: %s", e.httpCode, e.message)
 }
 
 // NewAPIError creates a new APIError instance from the given response code and content.
 func NewAPIError(code int, content []byte) error {
-	var errDef errorDefinition
+	errDef := &errorDef{httpCode: code}
 	switch {
 	case code == http.StatusBadRequest:
-		errDef = &errorDef{code: ErrCodeBadRequest}
+		errDef.errCode = ErrCodeBadRequest
 	case code == http.StatusUnauthorized:
-		errDef = &errorDef{code: ErrCodeUnauthorized}
+		errDef.errCode = ErrCodeUnauthorized
 	case code == http.StatusForbidden:
-		errDef = &errorDef{code: ErrCodeForbidden}
+		errDef.errCode = ErrCodeForbidden
 	case code == http.StatusNotFound:
-		errDef = &errorDef{code: ErrCodeNotFound}
+		errDef.errCode = ErrCodeNotFound
 	case code >= http.StatusInternalServerError:
-		errDef = &errorDef{code: ErrCodeServer}
+		errDef.errCode = ErrCodeServer
 	default:
-		errDef = &errorDef{code: ErrCodeUnknown}
+		errDef.errCode = ErrCodeUnknown
 	}
 	return parseContent(errDef, content)
 }
 
-type errorDefinition interface {
-	message() string
-	errCode() int
-}
-
-func parseContent(errDef errorDefinition, content []byte) error {
+func parseContent(errDef *errorDef, content []byte) error {
 	// If the content cannot be JSON-unmarshalled, we assume that it's not JSON
 	// and encode it into the APIError instance as-is.
 	errMessage := string(content)
@@ -66,14 +61,25 @@ func parseContent(errDef errorDefinition, content []byte) error {
 		errMessage = errDef.message()
 	}
 
-	return &APIError{message: errMessage, ErrCode: errDef.errCode()}
+	return &APIError{ErrCode: errDef.errCode, httpCode: errDef.httpCode, message: errMessage}
 }
 
 type errorDef struct {
-	ID      string                 `json:"error_id"`
-	Message string                 `json:"error_text"`
-	Details map[string]interface{} `json:"error_info"`
-	code    int
+	ID       string                 `json:"error_id"`
+	Message  string                 `json:"error_text"`
+	Details  map[string]interface{} `json:"error_info"`
+	httpCode int
+	errCode  int
+}
+
+func (def *errorDef) message() string {
+	message := def.Message
+	if len(def.Details) > 0 {
+		var details []string
+		parseMap("", def.Details, &details)
+		message = fmt.Sprintf("%s (%s)", message, strings.Join(details, ", "))
+	}
+	return message
 }
 
 func parseMap(prefix string, m map[string]interface{}, result *[]string) {
@@ -100,18 +106,4 @@ func parseMap(prefix string, m map[string]interface{}, result *[]string) {
 			}
 		}
 	}
-}
-
-func (def *errorDef) message() string {
-	message := def.Message
-	if len(def.Details) > 0 {
-		var details []string
-		parseMap("", def.Details, &details)
-		message = fmt.Sprintf("%s (%s)", message, strings.Join(details, ", "))
-	}
-	return message
-}
-
-func (def *errorDef) errCode() int {
-	return def.code
 }
--- error_test.go
+++ error_test.go
@@ -3,7 +3,6 @@ package vtm
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,28 +20,28 @@ func TestErrors(t *testing.T) {
 		{
 			httpCode: http.StatusBadRequest,
 			errCode:  ErrCodeBadRequest,
-			errText:  `The resource provided is invalid (basic.nodes_table."foo.bar.com:123".weight: Value '-1' must be within 1-100)`,
+			errText:  `VTM API error 400: The resource provided is invalid (basic.nodes_table."foo.bar.com:123".weight: Value '-1' must be within 1-100)`,
 			content:  content400(),
 		},
 		// 401
 		{
 			httpCode: http.StatusUnauthorized,
 			errCode:  ErrCodeUnauthorized,
-			errText:  "User name or password was invalid",
+			errText:  "VTM API error 401: User name or password was invalid",
 			content:  `{"error_id":"auth.invalid","error_text":"User name or password was invalid"}`,
 		},
 		// 403
 		{
 			httpCode: http.StatusForbidden,
 			errCode:  ErrCodeForbidden,
-			errText:  "Not Authorized to perform this action!",
+			errText:  "VTM API error 403: Not Authorized to perform this action!",
 			content:  `{"error_text": "Not Authorized to perform this action!"}`,
 		},
 		// 404
 		{
 			httpCode: http.StatusNotFound,
 			errCode:  ErrCodeNotFound,
-			errText:  "Resource '/api/tm/3.5/config/active/pools/my-pool' does not exist",
+			errText:  "VTM API error 404: Resource '/api/tm/3.5/config/active/pools/my-pool' does not exist",
 			content:  `{"error_id":"resource.not_found","error_text":"Resource '/api/tm/3.5/config/active/pools/my-pool' does not exist"}`,
 		},
 		// 499 unknown error
@@ -50,14 +49,14 @@ func TestErrors(t *testing.T) {
 			httpCode:   499,
 			nameSuffix: "unknown error",
 			errCode:    ErrCodeUnknown,
-			errText:    "unknown error",
+			errText:    "VTM API error 499: unknown error",
 			content:    `{"error_text": "unknown error"}`,
 		},
 		// 500
 		{
 			httpCode: http.StatusInternalServerError,
 			errCode:  ErrCodeServer,
-			errText:  "internal server error",
+			errText:  "VTM API error 500: internal server error",
 			content:  `{"error_text": "internal server error"}`,
 		},
 		// // 503 (no JSON)
@@ -65,7 +64,7 @@ func TestErrors(t *testing.T) {
 			httpCode:   http.StatusServiceUnavailable,
 			nameSuffix: "no JSON",
 			errCode:    ErrCodeServer,
-			errText:    "No server is available to handle this request.",
+			errText:    "VTM API error 503: No server is available to handle this request.",
 			content:    `No server is available to handle this request.`,
 		},
 	}
@@ -78,8 +77,7 @@ func TestErrors(t *testing.T) {
 		apiErr := NewAPIError(test.httpCode, []byte(test.content))
 		gotErrCode := apiErr.(*APIError).ErrCode
 		assert.Equal(t, test.errCode, gotErrCode, fmt.Sprintf("HTTP code %s (error code): got %d, want %d", name, gotErrCode, test.errCode))
-		pureErrText := strings.TrimPrefix(apiErr.Error(), "VTM API error: ")
-		assert.Equal(t, pureErrText, test.errText, fmt.Sprintf("HTTP code %s (error text)", name))
+		assert.Equal(t, apiErr.Error(), test.errText, fmt.Sprintf("HTTP code %s (error text)", name))
 	}
 }
 
